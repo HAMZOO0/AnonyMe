5a717cb80867ad708932c91b6114f0d8a76f03f8
Hamza <hamzakhan174722@gmail.com>
2025-08-02 21:14:30 +0500

    Rename VerificationEmail component to VerificationEmailTemplete and update import in sendVerificationEmail function; add ApiResponse type definition

--- src/app/helpers/sendVerficationEmail.ts
+++ src/app/helpers/sendVerficationEmail.ts
@@ -1,2 +1,26 @@
 import { resend } from "../lib/resend";
-import VerificationEmail from "../../../emails/verificationEmail";
+import VerificationEmailTemplete from "../../../emails/verificationEmail";
+import { ApiResponse } from "../lib/types/apiResponse";
+import { verify } from "crypto";
+
+export async function sendVerificationEmail(email: string, username: string, verifyCode: string): Promise<ApiResponse> {
+   try {
+      const { data, error } = await resend.emails.send({
+         from: "Acme <onboarding@resend.dev>",
+         to: email,
+         subject: "Feeback Project Verification Code",
+         react: VerificationEmailTemplete({ username, otp: verifyCode }), // otp is verification code
+      });
+
+      return {
+         status: true,
+         message: "Verification email send successfully.",
+      };
+   } catch (emailError) {
+      console.error("Error sending verification email:", emailError);
+      return {
+         status: false,
+         message: "Failed to send verification email.",
+      };
+   }
+}
