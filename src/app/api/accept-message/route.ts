b2fa7ed69d3373557fcf4b11ba9c7d55e6206ff9
Hamza <hamzakhan174722@gmail.com>
2025-08-30 19:07:12 +0500

    feat: implement GET and POST routes for message handling with user session validation and error responses

--- src/app/api/accept-message/route.ts
+++ src/app/api/accept-message/route.ts
@@ -4,6 +4,7 @@ import dbConnect from "@/app/lib/databaseConnect";
 import User from "next-auth";
 import UserModel from "@/model/user.model";
 
+// toggle user preference for accepting messages ON or OFF
 export async function POST(request: Request) {
    dbConnect();
    try {
@@ -30,11 +31,73 @@ export async function POST(request: Request) {
          { new: true }
       );
 
+      if (!updatedUser) {
+         return Response.json(
+            {
+               success: false,
+               message: "User not found",
+            },
+            {
+               status: 404,
+            }
+         );
+      }
+
       return Response.json(
          {
             success: true,
             message: "User preference updated successfully",
             user: updatedUser,
+         },
+         { status: 200 }
+      );
+   } catch (error) {
+      console.log("error :: ", error);
+      return Response.json(
+         {
+            success: false,
+            message: "Internal Server Error",
+         },
+         { status: 500 }
+      );
+   }
+}
+
+export async function GET(request: Request) {
+   try {
+      dbConnect();
+
+      //  here we can get the session
+      const session = await getServerSession(authOptions);
+      console.log("session :: ", session);
+
+      const user = session?.user;
+
+      // if no session
+      if (!session || !session.user) {
+         return Response.json({ message: "Unauthorized" }, { status: 401 });
+      }
+
+      // get user id from session
+      const userId = user?._id;
+
+      // fetch user
+      const existingUser = await UserModel.findById(userId).select("-password -verifiedCode -verifiedCodeExpiry");
+      if (!existingUser) {
+         return Response.json(
+            {
+               success: false,
+               message: "User not found",
+            },
+            {
+               status: 404,
+            }
+         );
+      }
+      return Response.json(
+         {
+            success: true,
+            isAcceptingMessage: existingUser?.isAcceptingMessage,
          },
          { status: 200 }
       );
---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,78 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
+import dbConnect from "@/app/lib/databaseConnect";
+import User from "next-auth";
+import UserModel from "@/model/user.model";
+import { Types } from "mongoose";
+
+export async function POST(request: Request) {
+   try {
+      dbConnect();
+
+      //  here we can get the session
+      const session = await getServerSession(authOptions);
+      console.log("session :: ", session);
+
+      const user = session?.user;
+
+      // if no session
+      if (!session || !session.user) {
+         return Response.json({ message: "Unauthorized" }, { status: 401 });
+      }
+
+      // get user id from session and convert to ObjectId
+      const userId = new Types.ObjectId(user?._id);
+
+      // find user by id  and aggregation to get only isAcceptingMessage field
+      const existingUser = await UserModel.aggregate([
+         {
+            // get the user by id
+            $match: { _id: userId },
+         },
+         {
+            // array into object
+            $unwind: "$messages",
+         },
+         {
+            // sort messages by createdAt in descending order
+            $sort: { "messages.createdAt": -1 },
+         },
+         {
+            // group back to user level and get only isAcceptingMessage field and messages array
+            $group: {
+               _id: "$_id",
+               messages: { $push: "$messages" },
+            },
+         },
+      ]);
+
+      if (!existingUser || existingUser.length === 0) {
+         return Response.json(
+            {
+               success: false,
+               message: "User not found",
+            },
+            { status: 404 }
+         );
+      }
+
+      console.log("existingUser :: ", existingUser);
+
+      return Response.json(
+         {
+            success: true,
+            messages: existingUser[0].messages,
+         },
+         { status: 200 }
+      );
+   } catch (error) {
+      console.log("error :: ", error);
+      return Response.json(
+         {
+            success: false,
+            message: "Internal Server Error",
+         },
+         { status: 500 }
+      );
+   }
+}
