2502e7a9a2f1d44d0cf5f4f95a8238bdd5146b18
Hamza <hamzakhan174722@gmail.com>
2025-09-08 18:44:37 +0500

    update delete route

--- src/app/api/delete-message/[id]/route.ts
+++ src/app/api/delete-message/[id]/route.ts
@@ -5,13 +5,13 @@ import User from "next-auth";
 import UserModel from "@/model/user.model";
 import { Types } from "mongoose";
 
-export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+export async function DELETE(request: Request, context: { params: { id: string } }) {
    try {
       dbConnect();
       const session = await getServerSession(authOptions);
       const user = session?.user;
 
-      const messageId = params.id;
+      const messageId = context.params.id;
       // console.log("Deleting message with id:", messageId);
 
       // if no session
